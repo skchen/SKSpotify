35245c9bc6a3dea97c2004d51dc1fc12b215cd72
Shin-Kai Chen <skchen@sixnology.com>
2016-04-20 13:49:11 +0800

    Add dependencies

---
+++ SKSpotify/Player/SKSpotifyPlayer.h
@@ -0,0 +1,13 @@
+//
+//  SKSpotifyPlayer.h
+//  SKSpotify
+//
+//  Created by Shin-Kai Chen on 2016/4/20.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+@import SKUtils;
+
+@interface SKSpotifyPlayer : SKPlayer
+
+@end
---
+++ SKSpotify/Player/SKSpotifyPlayer.m
@@ -0,0 +1,13 @@
+//
+//  SKSpotifyPlayer.m
+//  SKSpotify
+//
+//  Created by Shin-Kai Chen on 2016/4/20.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import "SKSpotifyPlayer.h"
+
+@implementation SKSpotifyPlayer
+
+@end
