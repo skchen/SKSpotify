4dadb5b217069ec79d453c4d256747a9ac946329
Shin-Kai Chen <skchen@sixnology.com>
2016-05-31 13:47:04 +0800

    Convert into workspace mode

---
+++ SKSpotify/Browse/SKSpotifyPagedList.h
@@ -0,0 +1,13 @@
+//
+//  SKSpotifyPagedList.h
+//  SKSpotify
+//
+//  Created by Shin-Kai Chen on 2016/5/31.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKSpotifyPagedList : NSObject
+
+@end
---
+++ SKSpotify/Browse/SKSpotifyPagedList.m
@@ -0,0 +1,13 @@
+//
+//  SKSpotifyPagedList.m
+//  SKSpotify
+//
+//  Created by Shin-Kai Chen on 2016/5/31.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import "SKSpotifyPagedList.h"
+
+@implementation SKSpotifyPagedList
+
+@end
